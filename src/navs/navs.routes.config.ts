babc0371a1a71f1e96d43912c91946d521d5f23c
kuldeep <kuldeepyadav7291@gmail.com>
2022-07-03 11:35:13 +0530

    Some refactoring

--- src/navs/navs.routes.config.ts
+++ src/navs/navs.routes.config.ts
@@ -3,13 +3,13 @@ import { CommonRoutesConfig } from '../common/common.routes.config';
 import { NavsRepository } from './navs.repository';
 
 export class NavsRouteConfig extends CommonRoutesConfig {
-  constructor(app: Application, name: string, private navRepo: NavsRepository) {
-    super(app, name);
-  }
+	constructor(app: Application, name: string, private navRepo: NavsRepository) {
+		super(app, name);
+	}
 
-  configureRoutes(): void {
-    this.app.route('/allNavs').get((request: Request, response: Response) => {
-      response.status(200).send(this.navRepo.getAllNavs());
-    });
-  }
+	configureRoutes(): void {
+		this.app.route('/allNavs').get((request: Request, response: Response) => {
+			response.status(200).send(this.navRepo.getAllNavs());
+		});
+	}
 }
